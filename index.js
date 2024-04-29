1da5d5910a4da206c4ab5e72870873430cb90d15
ardaberrun <ardaberrun@gmail.com>
2024-04-29 18:22:20 +0300

    add basic server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import http from 'http';
 import WeatherService from './weather.js';
 
 export const handler = async (event) => {
@@ -16,3 +17,28 @@ export const handler = async (event) => {
     };
   }
 };
+
+const server = http.createServer(async (req, res) => {
+  if (req.method !== 'GET') {
+    res.writeHead(405, { 'Allow': 'GET' }).end('Method Not Allowed');
+
+    return;
+  }
+
+  try {
+    const event = {
+      queryStringParameters: {
+        city: new URL(req.url, `http://${req.headers.host}`).searchParams.get('city') 
+      } 
+    };
+    const response = await handler(event);
+
+    res.writeHead(response.statusCode).end(JSON.stringify(response.body));
+  } catch (err) {
+    res.writeHead(500).end('ERROR');
+  }
+});
+
+server.listen(8000, '0.0.0.0', () => {
+  console.log('Server is running');
+});
